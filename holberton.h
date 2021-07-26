bdfe9170d2d90375aadda4d70473424fa3ec1670
dizzysnazzy <dismaskipchumba2@gmail.com>
2021-07-26 00:52:17 -0700

    2nd commit for whats needed for task 0

--- holberton.h
+++ holberton.h
@@ -4,6 +4,42 @@
 #include <stdlib.h>
 #include <stdarg.h>
 
+/**
+ * struct flags - struct containing flags to "turn on"
+ * when a flag specifier is passed to _printf()
+ * @plus: flag for the '+' character
+ * @space: flag for the ' ' character
+ * @hash: flag for the '#' character
+ */
+typedef struct flags
+{
+	int plus;
+	int space;
+	int hash;
+} flags_t;
+
+/**
+ * struct printHandler - struct to choose the right function depending
+ * on the format specifier passed to _printf()
+ * @c: format specifier
+ * @f: pointer to the correct printing function
+ */
+typedef struct printHandler
+{
+	char c;
+	int (*f)(va_list ap, flags_t *f);
+} ph;
+
+/* get_print */
+int (*get_print(char s))(va_list, flags_t *);
+
+/* get_flag */
+int get_flag(char s, flags_t *f);
+
+/* write_funcs */
+int _putchar(char c);
+int _puts(char *str);
+
 /* _printf */
 int _printf(const char *format, ...);
 
