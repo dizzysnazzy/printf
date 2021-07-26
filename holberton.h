374d767469a06da9d06201f64e7ea19142065921
dizzysnazzy <dismaskipchumba2@gmail.com>
2021-07-26 02:16:01 -0700

    print f project files

--- holberton.h
+++ holberton.h
@@ -43,9 +43,32 @@ int _puts(char *str);
 /* _printf */
 int _printf(const char *format, ...);
 
+/* converter */
+char *convert(unsigned long int num, int base, int lowercase);
+
+/* print_address */
+int print_address(va_list l, flags_t *f);
+
+/* print_nums */
+int print_int(va_list l, flags_t *f);
+void print_number(int n);
+int print_unsigned(va_list l, flags_t *f);
+int count_digit(int i);
+
 /* print_alpha */
 int print_string(va_list l, flags_t *f);
 int print_char(va_list l, flags_t *f);
+
+/* print_bases */
+int print_hex(va_list l, flags_t *f);
+int print_hex_big(va_list l, flags_t *f);
+int print_binary(va_list l, flags_t *f);
+int print_octal(va_list l, flags_t *f);
+
+/* print_custom */
+int print_rot13(va_list l, flags_t *f);
+int print_rev(va_list l, flags_t *f);
+int print_bigS(va_list l, flags_t *f);
 
 /* print_percent */
 int print_percent(va_list l, flags_t *f);
